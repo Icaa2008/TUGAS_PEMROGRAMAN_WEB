6202bcef00e1f25835b1b8914b660cc2ea5b7649
Icaa2008 <icaazara2008@gmail.com>
2025-12-12 21:11:29 +0700

    java script

---
+++ JAVA SCRIPT/1. profile_scrip.js
@@ -0,0 +1,15 @@
+let myName = "azzahra chesta raisya";
+const myAge = 17; 
+
+console.log("Nama:", myName);
+console.log("Umur:", myAge);
+
+
+document.getElementById("nama-saya").innerHTML = `Halo, Saya ${myName}`;
+document.getElementById("info-diri").innerHTML = `Saya berusia ${myAge} tahun dan suka belajar pemrograman.`;
+
+
+function showAlert() 
+{
+    alert("Anda telah mengklik tombol!");
+}
